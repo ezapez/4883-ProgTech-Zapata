924dd6af4076c460fc9263d407660ac3da831425
Edgar Z <70509722+ezapez@users.noreply.github.com>
2022-12-06 04:19:48 -0600

    update
    update

--- Assigments/A04/11172-relational-operator/main.py
+++ Assigments/A04/11172-relational-operator/main.py
@@ -1,17 +1,46 @@
+# ### keeps getting error end of file
+# while True:
+
+#   ### map to split the input to compare
+#   a,b = map(int,input().split())
+
+#   # ## compare what is bigger or smaller or equal
+#   # ### if statments to compare
+#   if a > b:
+#     print(">")
+#   elif a < b:
+#     print("<")
+#   else:
+#     print("=")
+
+#   t -= 1;
+
+
+
+
+
 import sys
-### to input files via terminal
-compare_nums =int(input())
+# ### to input files via terminal
 
+#resource to fix it
+#https://www.educba.com/python-eoferror/
+#https://stackoverflow.com/questions/42891603/how-to-remove-eoferror-eof-when-reading-a-line
+t = int(input())
 
-### loop
-for i in range(compare_nums):
-  ### map to split the input to compare
-  a, b = map(int, input().split())
-## compare what is bigger or smaller or equal
-### if statments to compare
-  if a > b:
-    print(">")
-  elif a < b:
-    print("<")
-  else:
-    print("=")
+while True:
+  try:
+    ### map to split the input to compare
+    a,b = map(int,input().split())
+
+    # ## compare what is bigger or smaller or equal
+    # ### if statments to compare
+    if a > b:
+      print(">")
+    elif a < b:
+     print("<")
+    else:
+      print("=")
+
+    t -= 1;
+  except:
+    break
