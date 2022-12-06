cc641c311d9cc6d3f2a5564d391871b242c1e95d
Edgar Z <70509722+ezapez@users.noreply.github.com>
2022-12-06 04:21:30 -0600

    update
    update

--- Assigments/A04/10346-petersmokes/main.py
+++ Assigments/A04/10346-petersmokes/main.py
@@ -1,21 +1,5 @@
-import sys
-### keeps getting error end of file 
-# # Read the input one line at a time
-# line = input()
-# while line:
-#     # Split the line into two numbers
-#     n, k = map(int, line.split())
 
-#     # Compute the maximum number of cigarettes that Peter can have
-#     cigarettes = n + (n - 1) // (k - 1)
-
-#     # Print the result
-#     print(cigarettes)
-
-#     # Read the next line of input
-#     line = input()
-
-#resource to fix it 
+#helper code to read in without end of file error
 #https://www.educba.com/python-eoferror/
 #https://stackoverflow.com/questions/42891603/how-to-remove-eoferror-eof-when-reading-a-line
 while True:
@@ -31,6 +15,3 @@ while True:
     n-=1
   except:
     break
-
-    
-
